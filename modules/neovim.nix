ccb23ecc28dfb2886cac0a9fe16ae569065dc6f8
Osi ic <osiic.offcl@gmail.com>
2025-08-19 22:06:02 +0700

    Update neovim.nix

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -3,16 +3,21 @@
 let
   nvimDir = "${config.home.homeDirectory}/.config/nvim";
 in {
+
   programs.neovim = {
     enable = true;
+    package = pkgs.neovim;       # Pakai satu Neovim saja
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
+
     extraConfig = ''
+      " Tambahkan runtimepath ke config lokal
       set runtimepath^=${nvimDir}
     '';
   };
 
+  # Pastikan folder nvim ada, clone atau pull repo
   home.activation.postActivation = ''
     if [ ! -d "${nvimDir}" ]; then
       git clone -b own https://github.com/osiic/nvim.git "${nvimDir}"
