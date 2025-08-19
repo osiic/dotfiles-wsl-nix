64b57391b5aeb310875ce6f3f2fda22721666611
Osi Ic <osiic.offcl@gmail.com>
2025-08-19 21:11:12 +0700

    update home

--- home.nix
+++ home.nix
@@ -1,11 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  # Ambil username & home directory otomatis
   home.username = builtins.getEnv "USER";
   home.homeDirectory = "/home/${builtins.getEnv "USER"}";
 
-  # Paket default
   home.packages = with pkgs; [
     git
     neovim
@@ -18,20 +16,9 @@
     starship
     nodejs_22
     python3Minimal
+    xclip
   ];
 
-  # Shell alias
-  programs.zsh.shellAliases = {
-    gs = "git status";
-    gl = "git log --oneline --graph --decorate";
-    v  = "nvim";
-    ll = "ls -alh";
-  };
-
-  # Starship prompt
-  programs.starship.enable = true;
-
-  # State version Home Manager
   home.stateVersion = "25.05";
 }
 
