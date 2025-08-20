b614a7cb07e1223e3a4144948be943a276a9bf36
Osi ic <osiic.offcl@gmail.com>
2025-08-20 14:27:27 +0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -7,22 +7,40 @@
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # Tmux config dari repo luar (bukan flake)
+    tmux-conf = {
+      url = "github:osiic/tmux";
+      flake = false;
+    };
+
+    # Neovim config dari repo luar juga (misal kamu punya `osiic/nvim`)
+    nvim-conf = {
+      url = "github:osiic/nvim";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, home-manager, tmux-conf, nvim-conf, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in {
       homeConfigurations.osiic = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-        modules = [ ./home.nix ];
-      };
+        modules = [
+          ./home.nix
+          ./modules/tmux.nix
+          ./modules/neovim.nix
 
-      # Biar bisa run: nix run .#switch
-      apps.${system}.switch = {
-        type = "app";
-        program = "${home-manager.packages.${system}.default}/bin/home-manager";
+          # inject inputs supaya bisa dipakai di modul
+          ({ ... }: {
+            _module.args = {
+              tmux-conf = tmux-conf;
+              nvim-conf = nvim-conf;
+            };
+          })
+        ];
       };
     };
 }
