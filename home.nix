7c00fd54dcace5e4be81d729d1f42071fd8c144a
Osi ic <osiic.offcl@gmail.com>
2025-08-20 14:06:31 +0700

    Update home.nix

--- home.nix
+++ home.nix
@@ -6,45 +6,19 @@
   programs.home-manager.enable = true;
 
   home.packages = with pkgs; [
-    # Startter
-    git
-    curl
-    wget
-    fzf
-    bat
-    eza
-    fd
+    # Starter
+    git curl wget fzf bat eza fd
 
-    # neovim
-    python3Minimal
-    lazygit
-    xclip
-    stylua
-    rust-analyzer
-    gcc
-    unzip
-    ripgrep
-    fd
-    go
-    rustc
-    luarocks
-    php
-    php84Packages.cyclonedx-php-composer
-    jdk
-    julia
-    python312Packages.pip
-    tree-sitter
-    nodejs_22
-    nil
-    nixpkgs-fmt
-    neovim-unwrapped
+    # Neovim deps
+    python3Minimal lazygit xclip stylua rust-analyzer gcc unzip ripgrep go
+    rustc luarocks php php84Packages.cyclonedx-php-composer jdk julia
+    python312Packages.pip tree-sitter nodejs_22 nil nixpkgs-fmt neovim-unwrapped
 
     # Tmux
     tmux
 
-    # shell
-    zsh
-    starship
+    # Shell
+    zsh starship
   ];
 
   imports = [
