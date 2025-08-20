b5c25b40ae72cbd9016e1d59979445b50fc0d8e2
Osi ic <osiic.offcl@gmail.com>
2025-08-20 14:16:52 +0700

    Update home.nix

--- home.nix
+++ home.nix
@@ -1,30 +1,61 @@
 { config, pkgs, lib, modulesPath, ... }:
 
 {
-  home.username = builtins.getEnv "USER";
-  home.homeDirectory = "/home/${builtins.getEnv "USER"}";
+  # User info
+  home.username = "osiic";
+  home.homeDirectory = "/home/osiic";
+
+  # Wajib: enable home-manager module
   programs.home-manager.enable = true;
 
+  # Paket yang mau di-install
   home.packages = with pkgs; [
-    # Starter
-    git curl wget fzf bat eza fd
+    # Starter tools
+    git
+    curl
+    wget
+    fzf
+    bat
+    eza
+    fd
 
-    # Neovim deps
-    python3Minimal lazygit xclip stylua rust-analyzer gcc unzip ripgrep go
-    rustc luarocks php php84Packages.cyclonedx-php-composer jdk julia
-    python312Packages.pip tree-sitter nodejs_22 nil nixpkgs-fmt neovim-unwrapped
+    # Neovim & deps
+    python3Minimal
+    lazygit
+    xclip
+    stylua
+    rust-analyzer
+    gcc
+    unzip
+    ripgrep
+    go
+    rustc
+    luarocks
+    php
+    php84Packages.cyclonedx-php-composer
+    jdk
+    julia
+    python312Packages.pip
+    tree-sitter
+    nodejs_22
+    nil
+    nixpkgs-fmt
+    neovim-unwrapped
 
     # Tmux
     tmux
 
     # Shell
-    zsh starship
+    zsh
+    starship
   ];
 
-  imports = [
-    ./modules/neovim.nix
-    ./modules/tmux.nix
-  ];
+  # Import module tambahan
+  # imports = [
+  #  ./modules/neovim.nix
+  #  ./modules/tmux.nix
+  # ];
 
+  # Versi state Home Manager (harus cocok sama channel yg dipakai)
   home.stateVersion = "24.11";
 }
